b834bc80889a426b3107c58834d6baee29a5d882
Pedro Augusto Mistieri Carvalho <pedroamcarvalho05@gmail.com>
2025-01-28 11:39:48 -0300

    Add files via upload

---
+++ secao8/Funcoes.java
@@ -0,0 +1,33 @@
+package secao8;
+
+public class Funcoes {
+    
+
+    public static void main(String[] args) {
+
+        // 1 - Crianddo a primeira função
+        // Tanto funções quanto variáveis a gente nomeia
+
+        saudacao();
+        saudacao();
+        saudacao();
+
+        
+    }
+
+    // Nome , os parenteses (ARGS), BLOCO {}
+   public static void saudacao() {
+
+    System.out.println("Olá, esta é minha primeira função!");
+    
+   } 
+
+
+    }
+
+
+    
+
+
+
+
