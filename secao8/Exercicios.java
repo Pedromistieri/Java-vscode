ef36c19c38f75d155e2ef2a3b8ee7ba0831e818d
Pedro Augusto Mistieri Carvalho <pedroamcarvalho05@gmail.com>
2025-02-04 16:36:14 -0300

    Add files via upload

--- secao8/Exercicios.java
+++ secao8/Exercicios.java
@@ -12,6 +12,18 @@ public class Exercicios {
     //Ex 2 Função para calcular fatorial de um número
     System.out.println("O fatorial de 10 é: " + calcularFatorial(10));
 
+    // Ex 3 Função com if-else para verificar Paridade
+
+    System.out.println(verificarParidade(10));
+    System.out.println(verificarParidade(101));
+
+    // Ex 4 Função com Switch para classificar notas
+    System.out.println(classificarNota(11));
+    System.out.println(classificarNota(10));
+    System.out.println(classificarNota(8));
+    System.out.println(classificarNota(3));
+
+
 
 
 
@@ -35,5 +47,41 @@ public class Exercicios {
         }
     }
 
+    public static String verificarParidade(int x) {
+
+        if(x % 2 == 0){
+            return " O número " + x + " é par! ";
+
+        }else{
+
+            return " O número " + x + " NÃO é par! ";
+
+
+        }
+
+
+    }
+
+        public static String classificarNota(int nota){
+
+            if(nota < 0 || nota > 10){
+                return "Nota invalida!";
+            }
+
+            switch (nota) {
+                case 10:
+                case 9:
+                  return "A";
+                case 8:  
+                  return "B";
+                case 7:  
+                  return "C";
+                case 6:  
+                  return "D";
+                 default:
+                    return "F";
+            }
+
+        }
 }
 
