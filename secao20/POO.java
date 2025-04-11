367143a9fb530666492958a043849a5cc3e067b4
Pedro Augusto Mistieri Carvalho <pedroamcarvalho05@gmail.com>
2025-04-11 18:25:51 -0300

    Delete secao20 directory

--- secao20/POO.java
+++
@@ -1,9 +0,0 @@
-package secao20;
-
-public class POO {
-    public static void main(String[] args) {
-        Motor motor1 = new Motor(450, "V8");
-        Carro carro1 = new Carro("Ford", "Mustang", motor1);
-        carro1.exibirInfo();
-    }
-}
